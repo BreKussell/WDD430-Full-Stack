eb9398098719a9505a0cba08409eef82013a848c
Breanna Russell <brekayussell@gmail.com>
2024-05-06 01:05:38 -0600

    w2 rework, doesn't need to be standalone

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'cms-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  standalone: true
+  
 })
 export class AppComponent {
   title = 'cms';
